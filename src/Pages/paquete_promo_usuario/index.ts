5eaec20b7029d9dc17651aeaed38f7ea51d1105c
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2024-01-23 02:45:55 -0400

    update

--- src/Pages/paquete_promo_usuario/index.ts
+++ src/Pages/paquete_promo_usuario/index.ts
@@ -1,12 +1,14 @@
 import { SPage, SPageListProps } from 'servisofts-component';
 import root from './root';
 import sucursales from './sucursales';
+import root_backup from './root_backup';
 export const Parent = {
     name: "paquete_promo_usuario",
     path: `/paquete_promo_usuario`,
 }
 export default SPage.combinePages(Parent.name, {
-    "": root,
+    // "": root,
+    "": root_backup,
     "sucursales": sucursales,
 
 });
