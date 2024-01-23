2f474f0b9b14c6e18715ef70cd67b279bc36b2cd
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2024-01-23 01:36:53 -0400

    update
    Se soluciono el bug de la publicacion
    se agrego una ventana sucursales, para que pase directo a confirma pago

--- src/Pages/paquete_promo_usuario/index.ts
+++ src/Pages/paquete_promo_usuario/index.ts
@@ -1,11 +1,12 @@
 import { SPage, SPageListProps } from 'servisofts-component';
-import root from './root2';
+import root from './root';
+import sucursales from './sucursales';
 export const Parent = {
     name: "paquete_promo_usuario",
     path: `/paquete_promo_usuario`,
 }
 export default SPage.combinePages(Parent.name, {
     "": root,
-    // "membresia": membresia,
+    "sucursales": sucursales,
 
 });
